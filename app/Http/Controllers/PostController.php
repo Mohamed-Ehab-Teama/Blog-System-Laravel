418cc10233021339640394643cb0fba874325f01
Mohamed Ehab Mohamed Teama <150657955+Mohamed-Ehab-Teama@users.noreply.github.com>
2024-10-08 17:36:55 +0300

    First Commit on Tuseday 8/10/2024 at 17:36

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    
+
+    public function index(){
+
+        $allPosts = [
+            [ 'id' => 1, 'title'=> 'PHP', 'created_by' => 'Mohamed Ehab', 'created_at' => '8/10/2024' ],
+            [ 'id' => 2, 'title'=> 'SQL', 'created_by' => ' Teama ', 'created_at' => '5/10/2024' ],
+            [ 'id' => 3, 'title'=> 'Laravel', 'created_by' => 'Mohamed', 'created_at' => '10/10/2024' ],
+            [ 'id' => 4, 'title'=> 'Docker', 'created_by' => 'Mohamed Ehab Mohamed Teama', 'created_at' => '25/10/2024' ],
+        ];
+
+        return view ('index', ['allPosts' => $allPosts]);
+
+    }
+
+}
---
+++ resources/views/index.blade.php
@@ -0,0 +1,77 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <title> All Posts </title>
+</head>
+
+<body>
+
+    <!-- Nav Bar -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#"> BLOG POST </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">All Posts</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <!-- Create Post Button -->
+    <div class="container text-center mt-5">
+        <button type="button" class="btn btn-success"> Create Post </button>
+    </div>
+
+
+    <!-- Posts Table -->
+    <div class="container mt-5">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Title</th>
+                    <th scope="col">Posted by</th>
+                    <th scope="col">Created at</th>
+                    <th scope="col">Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+
+                @foreach ($allPosts as $posts => $post )
+                <tr>
+                    <th scope="row"> {{$post['id']}} </th>
+                    <td> {{$post['title']}} </td>
+                    <td> {{$post['created_by']}} </td>
+                    <td> {{$post['created_at']}} </td>
+                    <td>
+                        <button type="button" class="btn btn-info"> View </button>
+                        <button type="button" class="btn btn-primary"> Edit </button>
+                        <button type="button" class="btn btn-danger"> Delete </button>
+                    </td>
+                </tr>
+                @endforeach
+
+            </tbody>
+        </table>
+    </div>
+
+
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
+
+</html>
