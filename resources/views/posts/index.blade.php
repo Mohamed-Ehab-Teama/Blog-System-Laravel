a2ab5c51c26921048253e2b216435a49d8031b31
Mohamed Ehab Mohamed Teama <150657955+Mohamed-Ehab-Teama@users.noreply.github.com>
2024-10-14 17:54:27 +0300

    Monday 14/10/2024 at 17:54 -- Template inheritance --

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,77 +1,48 @@
-<!doctype html>
-<html lang="en">
-
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-
-    <title> All Posts </title>
-</head>
-
-<body>
-
-    <!-- Nav Bar -->
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#"> BLOG POST </a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="{{route('posts.index')}}">All Posts</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
-
-    <!-- Create Post Button -->
-    <div class="container text-center mt-3">
-        <button type="button" class="btn btn-success"> Create Post </button>
-    </div>
+@extends('layouts.app')
 
 
-    <!-- Posts Table -->
-    <div class="container mt-3">
-        <table class="table">
-            <thead>
-                <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">Title</th>
-                    <th scope="col">Posted by</th>
-                    <th scope="col">Created at</th>
-                    <th scope="col">Actions</th>
-                </tr>
-            </thead>
-            <tbody>
+@section('title')
+    Index
+@endsection
 
-                @foreach ($allPosts as $posts => $post )
-                <tr>
-                    <th scope="row"> {{$post['id']}} </th>
-                    <td> {{$post['title']}} </td>
-                    <td> {{$post['created_by']}} </td>
-                    <td> {{$post['created_at']}} </td>
-                    <td>
-                        <a href="{{route('posts.show', $post['id'] )}}" class="btn btn-info"> View </a>
-                        <a href="#" class="btn btn-primary"> Edit </a>
-                        <a href="#" class="btn btn-danger"> Delete </a>
-                    </td>
-                </tr>
-                @endforeach
 
-            </tbody>
-        </table>
-    </div>
+@section('content')
 
 
 
+<!-- Create Post Button -->
+<div class="container text-center mt-3">
+    <button type="button" class="btn btn-success"> Create Post </button>
+</div>
+
+<table class="table mt-3">
+    <thead>
+        <tr>
+            <th scope="col">#</th>
+            <th scope="col">Title</th>
+            <th scope="col">Posted by</th>
+            <th scope="col">Created at</th>
+            <th scope="col">Actions</th>
+        </tr>
+    </thead>
+    <tbody>
+
+        @foreach ($allPosts as $posts => $post )
+        <tr>
+            <th scope="row"> {{$post['id']}} </th>
+            <td> {{$post['title']}} </td>
+            <td> {{$post['created_by']}} </td>
+            <td> {{$post['created_at']}} </td>
+            <td>
+                <a href="{{route('posts.show', $post['id'] )}}" class="btn btn-info"> View </a>
+                <a href="#" class="btn btn-primary"> Edit </a>
+                <a href="#" class="btn btn-danger"> Delete </a>
+            </td>
+        </tr>
+        @endforeach
+
+    </tbody>
+</table>
 
 
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-</body>
-
-</html>
+@endsection
