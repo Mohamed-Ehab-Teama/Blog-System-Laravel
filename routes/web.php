56cc987139df08c25979f1ec076e343149e33735
Mohamed Ehab Mohamed Teama <150657955+Mohamed-Ehab-Teama@users.noreply.github.com>
2024-10-17 11:44:56 +0300

    Thrusday 17/10/2024 at 11:44

--- routes/web.php
+++ routes/web.php
@@ -18,10 +18,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+// Show all Posts
 Route::get('/posts', [ PostController::class, 'index' ])->name('posts.index');
 
-Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
-
+// Store Post after creating it
 Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
 
+// Create Post [Go to the creation page]
+Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
+
+// Show Specific Post
 Route::get('/posts/{post}', [PostController::class, 'show'])->name('posts.show');
